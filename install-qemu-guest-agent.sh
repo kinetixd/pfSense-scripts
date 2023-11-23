0593c3a527294894bfe57c11a67df87d01488c92
David <david.cathcart@kinetix.net.au>
2023-11-23 17:51:55 +1000

    Removed debugging & enabled virtio console

--- install-qemu-guest-agent.sh
+++ install-qemu-guest-agent.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+echo Installing QEMU Agent
 pkg install -y qemu-guest-agent > /dev/null
 
+echo Activating startup configuration
 cat > /etc/rc.conf.local << EOF
 qemu_guest_agent_enable="YES"
-qemu_guest_agent_flags="-d -v -l /var/log/qemu-ga.log"
-#virtio_console_load="YES"
+qemu_guest_agent_flags="-v -l /var/log/qemu-ga.log"
+virtio_console_load="YES"
 EOF
 
 cat > /usr/local/etc/rc.d/qemu-agent.sh << EOF
@@ -16,4 +18,10 @@ EOF
 
 chmod +x /usr/local/etc/rc.d/qemu-agent.sh
 
-service qemu-guest-agent start
+echo Re-configuring agent
+sysrc qemu_guest_agent_enable="YES"
+sysrc qemu_guest_agent_flags="-v -l /var/log/qemu-ga.log"
+sysrc virtio_console_load="YES"
+
+echo Starting QEMU Agent
+service qemu-guest-agent start || service qemu-guest-agent restart
